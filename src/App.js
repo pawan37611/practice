afe8be2a4117284a85c4faef5893072547e09a4f
apple <apple@vs-MacBook-2.local>
2017-11-30 23:10:29 +0530

    refactor the code

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ class App extends Component {
     this.setValue = this.setValue.bind(this);
   }
 
-  previewFile() {
-    var preview = document.querySelector('img'); //selects the query named img
-    var file = document.querySelector('input[type=file]').files[0]; //sames as here
+  previewFile(e) {
+    var file = e.target.files[0]; //sames as here
     var reader = new FileReader();
 
     reader.onloadend = function () {
@@ -26,19 +25,16 @@ class App extends Component {
 
     if (file) {
       reader.readAsDataURL(file); //reads the data as a URL
-    } else {
-      preview.src = "";
     }
   }
    
   setValue(event) {
     var index = event.target.selectedIndex;
-    var optionElement = event.target.parentNode;
-    var key = optionElement.getAttribute('data-i');
-    var prev = optionElement.getAttribute('data-prev');
+    var key = event.target.getAttribute('data-i');
+    var prev = event.target.getAttribute('data-prev');
 
     if (!this.picArray.find((pic, index) => { return pic.val == event.target.value && index != key })) {
-      optionElement.setAttribute('data-prev', event.target.value);
+      event.target.setAttribute('data-prev', event.target.value);
       this.picArray[key].val = event.target.value;
 
     }
@@ -54,12 +50,12 @@ class App extends Component {
     return (
       <div>
         <p>Please Select Your Dress For the Day</p>
-        <input type="file" onChange={this.previewFile}/><br />
+        <input type="file" onChange={(e)=>{this.previewFile(e)}}/><br />
       <ul>{this.state.pics ? this.state.pics.map((pic, index) => {
           return (
-            <li key={index} data-i={index} data-prev={pic.val}>
-              <img src={pic.url} height="200" alt="Image preview..." />
-              <select onChange={this.setValue} >
+            <li key={index} >
+              <img src={pic.url}  height="200" alt="Image preview..." />
+              <select data-i={index} data-prev={pic.val} onChange={this.setValue} >
                 <option selected="selected" value="0">Please Select a Day</option>
                 <option value="1" >Monday</option>
                 <option value="2" >Tuesday</option>
